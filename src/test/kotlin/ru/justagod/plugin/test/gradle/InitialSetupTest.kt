5e6df52b4c86ba14a7993cdfd13843705fa36550
JustAGod <hvalick2111@gmail.com>
2021-12-02 16:52:57 +0300

    Code to publish to gradle central plugins repository

--- src/test/kotlin/ru/justagod/plugin/test/gradle/InitialSetupTest.kt
+++ src/test/kotlin/ru/justagod/plugin/test/gradle/InitialSetupTest.kt
@@ -1,12 +1,14 @@
 package ru.justagod.plugin.test.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.SourceSetOutput
+import org.gradle.api.tasks.TaskContainer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.BeforeEach
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.mockito.*
 import org.mockito.Mockito.*
+import org.mockito.kotlin.whenever
 import ru.justagod.cutter.processing.config.CutterConfig
 import ru.justagod.plugin.gradle.CutterPlugin
 import ru.justagod.plugin.gradle.CutterTask
@@ -23,6 +26,7 @@ class InitialSetupTest {
 
     @Mock
     lateinit var cutterTask: CutterTask
+
     @Mock
     lateinit var project: Project
 
@@ -35,8 +39,13 @@ class InitialSetupTest {
         doReturn(mock(ListProperty::class.java)).`when`(cutterTask).classPath
         doReturn(mock(Property::class.java)).`when`(cutterTask).config
 
-        `when`(project.configurations).thenReturn(mock(ConfigurationContainer::class.java))
+        val configurations = mock(ConfigurationContainer::class.java)
+        whenever(configurations.findByName(ArgumentMatchers.eq("compile"))).thenReturn(mock(Configuration::class.java))
+        `when`(project.configurations).thenReturn(configurations)
 
+        val tasks = mock(TaskContainer::class.java)
+        whenever(tasks.findByName(ArgumentMatchers.eq("build"))).thenReturn(mock(Task::class.java))
+        `when`(project.tasks).thenReturn(tasks)
 
         `when`(project.task(eq(mapOf("type" to CutterTask::class.java)), anyString())).thenAnswer {
             cutterTask
@@ -63,6 +72,7 @@ class InitialSetupTest {
 
         `when`(cutterTask.config).thenReturn(configProperty)
         val plugin = CutterPlugin()
+        plugin.javaClass.getDeclaredField("project").also { it.isAccessible = true }.set(plugin, project)
         CutterPlugin::class.java
             .getDeclaredMethod(methodName, Project::class.java)
             .also { it.isAccessible = true }
@@ -78,6 +88,7 @@ class InitialSetupTest {
     fun tasksAdded() {
         `when`(cutterTask.config).thenReturn(mock(Property::class.java) as Property<CutterConfig>)
         val plugin = CutterPlugin()
+        plugin.javaClass.getDeclaredField("project").also { it.isAccessible = true }.set(plugin, project)
         CutterPlugin::class.java
             .getDeclaredMethod("initiateTasks", Project::class.java)
             .also { it.isAccessible = true }
@@ -85,8 +96,14 @@ class InitialSetupTest {
 
 
 
-        verify(project).task(ArgumentMatchers.eq(mapOf("type" to CutterTask::class.java)), ArgumentMatchers.eq("buildServer"))
-        verify(project).task(ArgumentMatchers.eq(mapOf("type" to CutterTask::class.java)), ArgumentMatchers.eq("buildClient"))
+        verify(project).task(
+            ArgumentMatchers.eq(mapOf("type" to CutterTask::class.java)),
+            ArgumentMatchers.eq("buildServer")
+        )
+        verify(project).task(
+            ArgumentMatchers.eq(mapOf("type" to CutterTask::class.java)),
+            ArgumentMatchers.eq("buildClient")
+        )
     }
 
 }
